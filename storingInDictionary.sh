303c92e313fe0b429381bed0001cc2edbcb0f4a1
unknown <jimpidas>
2021-04-05 23:18:52 +0530

    Added the code for UC6

---
+++ storingInDictionary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+echo "Enter 3 Numbers : "
+read a b c
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+declare -A result
+result[uc2]=$result1
+result[uc3]=$result2
+result[uc4]=$result3
+result[uc5]=$result4
+
