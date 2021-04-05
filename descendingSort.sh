c9db3b3759cf80252750696fbc851020f0eb4263
unknown <jimpidas>
2021-04-05 23:21:41 +0530

    Added the code for UC8

---
+++ descendingSort.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+echo "Enter 3 Numbers : "
+read a b c
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+declare -A result
+counter=0
+result[$(($counter))]=$result1
+result[$(($counter+1))]=$result2
+result[$(($counter+2))]=$result3
+result[$(($counter+3))]=$result4
+
+echo ${result[@]}
+echo ${!result[@]}
+for key in ${!result[@]}
+do
+        arr[$key]=${result[$key]}
+done
+
+for ((i=0; i < ${#arr[@]}; i++))
+do
+	for ((j=i+1; j < ${#arr[@]}; j++))
+	do
+		if [ ${arr[i]} -lt ${arr[j]} ]
+		then
+			temp=${arr[i]}
+			arr[$i]=${arr[j]}
+			arr[$j]=$temp
+		fi
+	done
+done
+echo "The numbers in descending orders are :" ${arr[@]}
